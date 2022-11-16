5e3d10294d9d0fa29a430ad72012397588fca94b
olegechek <abba1111@yandex.ru>
2022-11-16 22:49:38 +0300

    Сделал отрисуй меня полностью часть 1

---
+++ js/gallery.js
@@ -0,0 +1,36 @@
+import { getPhotoObjectArray } from './data.js'
+
+const createPhotoArray = getPhotoObjectArray();
+
+const picturesList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const pictureListFragment = document.createDocumentFragment();
+
+createPhotoArray.forEach(({ url, likes, comments }) => {
+
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureListFragment.appendChild(pictureElement);
+});
+
+picturesList.appendChild(pictureListFragment);
+
+
+
+
+/*
+// мой вариант - более простой
+for (let i = 0; i < createPhotoArray.length; i++) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = createPhotoArray[i].url;
+  pictureElement.querySelector('.picture__comments').textContent = createPhotoArray[i].comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = createPhotoArray[i].likes;
+
+  pictureListFragment.appendChild(pictureElement);
+}
+
+picturesList.appendChild(pictureListFragment);
+*/
